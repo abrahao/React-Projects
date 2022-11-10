dffafe836a6f6834b2f481b27a107163e23ddab5
Abrahao Eneias <abrahao.eneias@dce.ufpb.br>
2022-11-10 20:27:29 -0300

    Update

--- Desafios/desafio02-github-wiki/src/index.js
+++ Desafios/desafio02-github-wiki/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/App';
+import GlobalStyles from "./styles";
 
-import { GlobalStyles } from "./styles";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyles/>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
+
--- Desafios/desafio02-github-wiki/src/styles.js
+++ Desafios/desafio02-github-wiki/src/styles.js
@@ -1,12 +1,16 @@
-import { createGlobalStyle } from 'styled-components'
+import {createGlobalStyle} from 'styled-components';
+
 
 export default createGlobalStyle`
-  *, body {
+
+*, body{
     margin: 0;
     padding: 0;
-  }
-  body{
-    background-color: #22272e;
-    color: #FFFFFF
-  }
-  `;
+}
+
+body {
+    background-color: #0d1117;
+    color: #FFFFFF;
+}
+
+`
